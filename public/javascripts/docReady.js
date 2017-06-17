35ddcc33fe3cff5025689bbc2f44e7268cb8f5a2
Bryan <starwarsgeek0975@gmail.com>
2017-06-16 23:55:37 -0600

    first commit

---
+++ public/javascripts/docReady.js
@@ -0,0 +1,10 @@
+/**
+ * Created by starw on 6/16/2017.
+ */
+$( document ).ready(function(){
+  $(".button-collapse").sideNav();
+  $(".menu").sideNav();
+  // Initialize collapse button
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  //$('.collapsible').collapsible();
+});
