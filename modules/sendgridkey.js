69e686147027f7a15c610945a35d7b8292236ad8
Bryan <starwarsgeek0975@gmail.com>
2017-07-11 20:50:19 -0600

    added basic email web end point

---
+++ modules/sendgridkey.js
@@ -0,0 +1,6 @@
+/**
+ * Created by starw on 7/11/2017.
+ */
+
+const key = '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';
+module.exports = key;
--- routes/friends.js
+++ routes/friends.js
@@ -70,4 +70,30 @@ router.get('/getFriends', (req, res, next) => {
   }
 });
 
+
+router.get('/invite', (req, res, next) => {
+  const sendgrid_api_key = require('../modules/sendgridkey');
+  var helper = require('sendgrid').mail;
+  var fromEmail = new helper.Email('donotreply@chat-material.com');
+  var toEmail = new helper.Email('mando0975@gmail.com');
+  var subject = 'Sending with SendGrid is Fun';
+  var content = new helper.Content('text/plain', 'and easy to do anywhere, even with Node.js');
+  var mail = new helper.Mail(fromEmail, subject, toEmail, content);
+
+  var sg = require('sendgrid')(sendgrid_api_key);
+  var request = sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: mail.toJSON()
+  });
+
+  sg.API(request, function (error, response) {
+    if (error) {
+      console.log('Error response received');
+    }
+    console.log(response.statusCode);
+    console.log(response.body);
+    console.log(response.headers);
+  });
+});
 module.exports = router;
